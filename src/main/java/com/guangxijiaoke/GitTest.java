549de37494b9c2b3670dbddb03e006970a83e99b
huangpeiming <peiming_huang@163.com>
2022-08-01 11:10:07 +0800

    first commit

---
+++ src/main/java/com/guangxijiaoke/GitTest.java
@@ -0,0 +1,11 @@
+package com.guangxijiaoke;
+
+/**
+ * @author huangpeiming
+ * @create 2022-08-01 11:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
